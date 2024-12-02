f5d20ecaec1283096e1fd9aa2808cb9f26fd85d5
TTibbs <terryjw.97@gmail.com>
2024-12-02 12:56:57 +0000

    FE fix?

--- src/app.js
+++ src/app.js
@@ -9,7 +9,14 @@ const {
   inputErrorHandler,
 } = require("./errors/index.js");
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 app.use("/api", apiRouter);
